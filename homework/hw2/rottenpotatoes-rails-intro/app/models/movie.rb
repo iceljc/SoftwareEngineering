5896809a4ea607aa931f2aab786ef8ca168cb936
iceljc <46039045+iceljc@users.noreply.github.com>
2019-02-11 14:01:35 -0600

    - complete

--- homework/hw2/rottenpotatoes-rails-intro/app/models/movie.rb
+++ homework/hw2/rottenpotatoes-rails-intro/app/models/movie.rb
@@ -1,2 +1,20 @@
 class Movie < ActiveRecord::Base
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+
+    def self.all_ratings 
+        @all_ratings
+    end
+    
+    def self.sorting(sort)
+        if sort == nil
+            self
+        else
+            self.order(sort)
+        end
+    end
+    
+    def self.with_rating(ratings)
+        self.where(rating: ratings)
+        # self.where("rating in (?)", ratings)
+    end
 end
